a3a75b18304bf40c042dcd01a8aa0b62a683ca71
solomon54 <solomontsehay54@gmail.com>
2026-01-17 23:39:49 +0300

    include the missing files

--- prodev-mobile-app-0x06/tailwind.config.js
+++ prodev-mobile-app-0x06/tailwind.config.js
@@ -1,10 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
     "./styles/**/*.{js,jsx,ts,tsx}",
   ],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
